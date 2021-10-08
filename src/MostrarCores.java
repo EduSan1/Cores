77494a4a70ef69790985b571cfd24883dd7c4344
EDUARDO SANTOS NASCIMENTO <21276323@CFP127ALUNO.LOCAL>
2021-10-08 16:41:35 -0300

    feito

---
+++ src/MostrarCores.java
@@ -0,0 +1,49 @@
+import java.util.Scanner;
+
+public class MostrarCores {
+
+	public static void main(String[] args) {
+
+		int naoEncontrado = 1;
+		String cores[] = new String[10];
+		cores[0] = "Azul";
+		cores[1] = "Amarelo";
+		cores[2] = "Vermelho";
+		cores[3] = "Lilás";
+		cores[4] = "Roxo";
+		cores[5] = "Laranja";
+		cores[6] = "Preto";
+		cores[7] = "Verde";
+		cores[8] = "Cinza";
+		cores[9] = "Branco";
+
+		System.out.print("Digite a cor que vc deseja:");
+		Scanner leitor = new Scanner(System.in);
+		String corEscolhida = leitor.next();
+		leitor.close();
+
+		for (int contador = 0; contador < cores.length; contador++) {
+
+			if (corEscolhida.equalsIgnoreCase(cores[contador])) {
+
+				System.out.println("A cor " + cores[contador] + " foi achada em nosso sistema!");
+				naoEncontrado = 0;
+				contador = cores.length;
+
+			} else {
+
+			}
+
+		}
+
+		if (naoEncontrado == 1) {
+
+			System.out.println("Não encontramos sua cor...");
+
+		} else {
+
+		}
+
+	}
+
+}
---
+++ src/TesteArray.java
@@ -0,0 +1,37 @@
+
+public class TesteArray {
+
+	public static void main(String[] args) {
+		
+//		int notas[] = new int[4];
+//		
+//		notas[0] = 8;
+//		notas[1] = 7;
+//		notas[2] = 10;
+//		notas[3] = 6;
+//		
+//		System.out.println(notas[2] + notas[0]);
+		
+		String linguagens[] = new String[10];
+		linguagens[0] = "Java";
+		linguagens[1] = "C#";
+		linguagens[2] = "PHP";
+		linguagens[3] = "Ruby";
+		linguagens[4] = "Kotlin";
+		linguagens[5] = "Fortran";
+		linguagens[6] = "Cobal";
+		linguagens[7] = "Pascal";
+		linguagens[8] = "JavaScript";
+		linguagens[9] = "Objective-c";
+		
+		
+		for (int contador = 0; contador < linguagens.length ; contador++) {
+			
+			System.out.println(linguagens[contador]);
+			
+			
+		}
+		
+	}
+
+}
